b37c48a890408342857df5d4d31275978987cda4
BenjaminNussbaumer <benjamin@dschool.fr>
2017-02-01 15:28:37 +0100

    Avant fetched

--- ListBen/ViewController.swift
+++ ListBen/ViewController.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+//Add below "import UIKit"
+import CoreData
 
 class ViewController: UIViewController, UITableViewDataSource {
 
     
     @IBOutlet weak var tableView: UITableView!
     
-    //Insert below the tableView IBOutlet
-    var names = [String]()
+    //Change “names” to “people” and [String] to [NSManagedObject]
+    var people = [NSManagedObject]()
+    
     
     
     @IBAction func addName(_ sender: Any) {
@@ -29,9 +32,13 @@ class ViewController: UIViewController, UITableViewDataSource {
                                        handler: { (action:UIAlertAction) -> Void in
                                         
                                         let textField = alert.textFields!.first
-                                        self.names.append(textField!.text!)
+                                        self.saveName(name: textField!.text!)
                                         self.tableView.reloadData()
+                                        
+                                        
+                                        
         })
+    
         
         let cancelAction = UIAlertAction(title: "Cancel",
                                          style: .default) { (action: UIAlertAction) -> Void in
@@ -58,11 +65,10 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
 
     
-
-    // MARK: UITableViewDataSource
+    //tableview
     func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     
     func tableView(_ tableView: UITableView,
@@ -72,18 +78,51 @@ class ViewController: UIViewController, UITableViewDataSource {
         let cell =
             tableView.dequeueReusableCell(withIdentifier: "Cell")
         
-        cell!.textLabel!.text = names[indexPath.row]
+        let person = people[indexPath.row]
+        
+        cell!.textLabel!.text =
+            person.value(forKey: "name") as? String
         
         return cell!
     }
-    
-    
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    //SaveName
+    func saveName(name: String) {
+        //1
+        let appDelegate =
+            UIApplication.shared.delegate as! AppDelegate
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+     
+        
+        //2
+        let entity =  NSEntityDescription.entity(forEntityName: "Person",
+                                                 in:managedContext)
+        
+        let person = NSManagedObject(entity: entity!,
+                                     insertInto: managedContext)
+        
+        //3
+        person.setValue(name, forKey: "name")
+        
+        //4
+        do {
+            try managedContext.save()
+            //5
+            people.append(person)
+        } catch let error as NSError  {
+            print("Could not save \(error), \(error.userInfo)")
+        }
+    }
+    
+    
+    
 
 }
 
